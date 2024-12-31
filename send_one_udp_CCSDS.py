e8664ce9fca895f5f00604b938264a8fe07d7ce2
junfuture1103 <junhak1103@naver.com>
2024-12-30 22:45:58 -0800

    init StateAware Function ReadMe.md

--- send_one_udp_CCSDS.py
+++ send_one_udp_CCSDS.py
@@ -2,7 +2,7 @@ import socket
 
 # 대상 IP와 포트 설정
 target_ip = "127.0.0.1"
-target_port = 1234
+target_port = 1235
 
 # 헥스 문자열을 입력으로 받아 바이트 배열로 변환하는 함수
 def parse_hex_string(hex_string):
@@ -10,7 +10,7 @@ def parse_hex_string(hex_string):
     return bytes(int(value, 16) for value in hex_values)  # 각 값을 16진수로 변환하여 바이트 배열 생성
 
 # 사용자로부터 헥스 문자열 입력
-hex_string = "18 09 C0 00 00 01 00 00"  # 예시로 들어가는 문자열
+hex_string = "41 41 41"  # 예시로 들어가는 문자열
 hex_data = parse_hex_string(hex_string)  # 헥스 문자열을 바이트 배열로 변환
 
 # 소켓 생성 및 데이터 전송
--- test.py
+++
@@ -1,6 +0,0 @@
-import random
-
-
-cmd_ind = random.randint(0, 2)
-
-print(cmd_ind)
